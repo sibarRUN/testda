685bffa017bd87983049acc0e0f6559283e85d45
hugururu <sspp06036@gmail.com>
2024-12-10 11:28:50 +0900

    v1.4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -97,8 +97,8 @@ const Item = styled(motion.li)`
 
 // Cognito User Pool 설정
 const poolData = {
-  UserPoolId: 'ap-northeast-2_jczobrwlq', // 사용자 풀 ID
-  ClientId: 'e90hcf6rica8am3h81lcsuspe',  // 앱 클라이언트 ID
+  UserPoolId: 'YOUR_USER_POOL_ID', // 사용자 풀 ID
+  ClientId: 'YOUR_APP_CLIENT_ID',  // 앱 클라이언트 ID
 };
 
 const userPool = new CognitoUserPool(poolData);
@@ -137,6 +137,24 @@ const Navbar = () => {
       duration: '500',
       easing: [0.25, 0.0, 0.35, 1.0],
     });
+  };
+
+  const handleBonggenieClick = () => {
+    const user = userPool.getCurrentUser(); // 현재 사용자 가져오기
+    if (user) {
+      user.getSession((err, session) => {
+        if (err || !session.isValid()) {
+          // 세션이 유효하지 않으면 로그인 페이지로 리디렉션
+          window.location.href = 'https://ap-northeast-2jczobrwlq.auth.ap-northeast-2.amazoncognito.com/login?client_id=e90hcf6rica8am3h81lcsuspe&response_type=code&scope=email+openid&redirect_uri=https%3A%2F%2Fd2u50llkglor25.cloudfront.net';
+        } else {
+          // 세션이 유효하면 BONGGENIE 페이지로 이동
+          window.location.href = '/bonggenie';
+        }
+      });
+    } else {
+      // 사용자가 없으면 로그인 페이지로 리디렉션
+      window.location.href = 'https://ap-northeast-2jczobrwlq.auth.ap-northeast-2.amazoncognito.com/login?client_id=e90hcf6rica8am3h81lcsuspe&response_type=code&scope=email+openid&redirect_uri=https%3A%2F%2Fd2u50llkglor25.cloudfront.net';
+    }
   };
 
   return (
@@ -189,7 +207,7 @@ const Navbar = () => {
         <Item
           whileHover={{ scale: 1.1, y: -5 }}
           whileTap={{ scale: 0.9, y: 0 }}
-          onClick={() => window.location.href = '/bongjini.html'}
+          onClick={handleBonggenieClick}
         >
           <Link to="#">BonGenie</Link>
         </Item>
