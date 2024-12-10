a787035f4a993a6ee54dbbf9f71e78f54deefac9
hugururu <sspp06036@gmail.com>
2024-12-10 11:47:11 +0900

    V 2.0.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,15 @@
 import { motion } from 'framer-motion';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useLocomotiveScroll } from 'react-locomotive-scroll';
 import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
-
+// 네비게이션 컨테이너 스타일 정의
 const NavContainer = styled(motion.div)`
   position: absolute;
-  /* left: 50%; */
   top: ${(props) => (props.click ? '0' : `-${props.theme.navHeight}`)};
   transition: all 0.3s ease;
-  /* transform: translateX(-50%); */
   z-index: 6;
   width: 100vw;
 
@@ -19,36 +17,26 @@ const NavContainer = styled(motion.div)`
   justify-content: center;
   align-items: center;
 
-  
   @media (max-width: 40em) {
     top: ${(props) => (props.click ? '0' : `calc(-50vh - 4rem)`)};
-
   }
 `;
 
+// 메뉴 버튼 스타일 정의
 const MenuBtn = styled.li`
   background-color: ${(props) => `rgba(${props.theme.textRgba},0.7)`};
   color: ${(props) => props.theme.body};
   width: 15rem;
   height: 2.5rem;
-
   border: none;
   outline: none;
-
   clip-path: polygon(0 0, 100% 0, 80% 100%, 20% 100%);
-
   position: absolute;
   top: 100%;
   left: 50%;
   transform: translateX(-50%);
-
   font-size: ${(props) => props.theme.fontmd};
   font-weight: 600;
-
-  /* border-end-start-radius: 50%; */
-
-  /* border-end-end-radius: 50%; */
-
   cursor: pointer;
 
   display: flex;
@@ -60,10 +48,10 @@ const MenuBtn = styled.li`
   @media (max-width: 40em) {
     width: 10rem;
     height: 2rem;
-
   }
 `;
 
+// 메뉴 아이템 스타일 정의
 const MenuItems = styled(motion.ul)`
   position: relative;
   height: ${(props) => props.theme.navHeight};
@@ -78,59 +66,39 @@ const MenuItems = styled(motion.ul)`
   padding: 0 10rem;
 
   @media (max-width: 40em) {
-    flex-direction:column;
-    padding:2rem 0;
+    flex-direction: column;
+    padding: 2rem 0;
     height: 50vh;
   }
 `;
 
+// 개별 메뉴 아이템 스타일 정의
 const Item = styled(motion.li)`
   text-transform: uppercase;
   color: ${(props) => props.theme.text};
 
   @media (max-width: 40em) {
-    flex-direction:column;
-    padding:0.5rem 0;
-
+    padding: 0.5rem 0;
   }
 `;
 
 // Cognito User Pool 설정
 const poolData = {
-  UserPoolId: 'YOUR_USER_POOL_ID', // 사용자 풀 ID
-  ClientId: 'YOUR_APP_CLIENT_ID',  // 앱 클라이언트 ID
+  UserPoolId: 'ap-northeast-2_jczobrwlq', // 사용자 풀 ID
+  ClientId: 'e90hcf6rica8am3h81lcsuspe',  // 앱 클라이언트 ID
 };
 
 const userPool = new CognitoUserPool(poolData);
 
 const Navbar = () => {
-  const [click, setClick] = useState(false);
+  const [click, setClick] = useState(false); // 메뉴 클릭 상태 관리
 
-  const { scroll } = useLocomotiveScroll();
-  const navigate = useNavigate();
+  const { scroll } = useLocomotiveScroll(); // 스크롤 기능 사용
+  const navigate = useNavigate(); // 페이지 이동 기능 사용
 
-  useEffect(() => {
-    const checkAuth = () => {
-      const user = userPool.getCurrentUser(); // 현재 사용자 가져오기
-      if (user) {
-        user.getSession((err, session) => {
-          if (err || !session.isValid()) {
-            // 세션이 유효하지 않으면 로그인 페이지로 리디렉션
-            window.location.href = 'https://ap-northeast-2jczobrwlq.auth.ap-northeast-2.amazoncognito.com/login?client_id=e90hcf6rica8am3h81lcsuspe&response_type=code&scope=email+openid&redirect_uri=https%3A%2F%2Fd2u50llkglor25.cloudfront.net';
-          }
-        });
-      } else {
-        // 사용자가 없으면 로그인 페이지로 리디렉션
-        window.location.href = 'https://ap-northeast-2jczobrwlq.auth.ap-northeast-2.amazoncognito.com/login?client_id=e90hcf6rica8am3h81lcsuspe&response_type=code&scope=email+openid&redirect_uri=https%3A%2F%2Fd2u50llkglor25.cloudfront.net';
-      }
-    };
-
-    checkAuth(); // 인증 상태 확인
-  }, []);
-
+  // 특정 요소로 스크롤하는 함수
   const handleScroll = (id) => {
-    let elem = document.querySelector(id);
-    // console.log(elem);
+    const elem = document.querySelector(id);
     setClick(!click);
     scroll.scrollTo(elem, {
       offset: '-100',
@@ -139,12 +107,15 @@ const Navbar = () => {
     });
   };
 
-  const handleBonggenieClick = () => {
-    const user = userPool.getCurrentUser(); // 현재 사용자 가져오기
+  // 봉지니 버튼 클릭 시 처리 함수
+  const handleBonggenieClick = (event) => {
+    event.preventDefault();
+    const user = userPool.getCurrentUser();
     if (user) {
       user.getSession((err, session) => {
         if (err || !session.isValid()) {
-          // 세션이 유효하지 않으면 로그인 페이지로 리디렉션
+          // 세션이 유효하지 않으면 알람 후 로그인 페이지로 리디렉션
+          alert("봉지니는 로그인 후 사용하실 수 있습니다.");
           window.location.href = 'https://ap-northeast-2jczobrwlq.auth.ap-northeast-2.amazoncognito.com/login?client_id=e90hcf6rica8am3h81lcsuspe&response_type=code&scope=email+openid&redirect_uri=https%3A%2F%2Fd2u50llkglor25.cloudfront.net';
         } else {
           // 세션이 유효하면 BONGGENIE 페이지로 이동
@@ -152,7 +123,8 @@ const Navbar = () => {
         }
       });
     } else {
-      // 사용자가 없으면 로그인 페이지로 리디렉션
+      // 사용자가 없으면 알람 후 로그인 페이지로 리디렉션
+      alert("봉지니는 로그인 후 사용하실 수 있습니다.");
       window.location.href = 'https://ap-northeast-2jczobrwlq.auth.ap-northeast-2.amazoncognito.com/login?client_id=e90hcf6rica8am3h81lcsuspe&response_type=code&scope=email+openid&redirect_uri=https%3A%2F%2Fd2u50llkglor25.cloudfront.net';
     }
   };
@@ -162,7 +134,7 @@ const Navbar = () => {
       click={+click}
       initial={{ y: `-100%` }}
       animate={{ y: 0 }}
-      transition={{ duration: 0.5, delay: 0 /* 2 */ }}
+      transition={{ duration: 0.5, delay: 0 }}
     >
       <MenuItems
         drag="y"
@@ -178,7 +150,6 @@ const Navbar = () => {
           whileTap={{ scale: 0.9, y: 0 }}
           onClick={() => handleScroll('#home')}
         >
-          {' '}
           <Link to="/">Home</Link>
         </Item>
         <Item
@@ -186,16 +157,15 @@ const Navbar = () => {
           whileTap={{ scale: 0.9, y: 0 }}
           onClick={() => handleScroll('.about')}
         >
-          <Link to="/">about</Link>
+          <Link to="/">About</Link>
         </Item>
         <Item
           whileHover={{ scale: 1.1, y: -5 }}
           whileTap={{ scale: 0.9, y: 0 }}
           onClick={() => handleScroll('#shop')}
         >
-          <Link to="/">shop</Link>
+          <Link to="/">Shop</Link>
         </Item>
-
         <Item
           whileHover={{ scale: 1.1, y: -5 }}
           whileTap={{ scale: 0.9, y: 0 }}
@@ -203,7 +173,6 @@ const Navbar = () => {
         >
           <Link to="#">Login / Register</Link>
         </Item>
-
         <Item
           whileHover={{ scale: 1.1, y: -5 }}
           whileTap={{ scale: 0.9, y: 0 }}
