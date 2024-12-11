1ba381afa14d2635ca288ebe1d2bd11c267089ad
hugururu <sspp06036@gmail.com>
2024-12-11 11:04:12 +0900

    v3.6.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,7 @@ import {
   CognitoRefreshToken
 } from 'amazon-cognito-identity-js';
 
+// 스타일링: 네비게이션 컨테이너
 const NavContainer = styled(motion.div)`
   position: absolute;
   top: ${(props) => (props.click ? '0' : `-${props.theme.navHeight}`)};
@@ -27,6 +28,7 @@ const NavContainer = styled(motion.div)`
   }
 `;
 
+// 스타일링: 메뉴 버튼
 const MenuBtn = styled.li`
   background-color: ${(props) => `rgba(${props.theme.textRgba},0.7)`};
   color: ${(props) => props.theme.body};
@@ -53,6 +55,7 @@ const MenuBtn = styled.li`
   }
 `;
 
+// 스타일링: 메뉴 항목 리스트
 const MenuItems = styled(motion.ul)`
   position: relative;
   height: ${(props) => props.theme.navHeight};
@@ -72,6 +75,7 @@ const MenuItems = styled(motion.ul)`
   }
 `;
 
+// 스타일링: 개별 메뉴 항목
 const Item = styled(motion.li)`
   text-transform: uppercase;
   color: ${(props) => props.theme.text};
@@ -81,27 +85,36 @@ const Item = styled(motion.li)`
   }
 `;
 
+// Cognito User Pool 설정
 const poolData = {
-  UserPoolId: 'ap-northeast-2_jczobrwlq', // 실제 User Pool ID
+  UserPoolId: 'ap-northeast-2_jczobrwlq', // 실제 Cognito User Pool ID
   ClientId: 'e90hcf6rica8am3h81lcsuspe',  // 실제 App Client ID
 };
 
+// Cognito User Pool 객체 생성
 const userPool = new CognitoUserPool(poolData);
 
 const Navbar = () => {
+  // 메뉴 클릭 상태 관리
   const [click, setClick] = useState(false);
+
+  // LocomotiveScroll 라이브러리 사용
   const { scroll } = useLocomotiveScroll();
+
+  // React Router를 사용한 페이지 이동
   const navigate = useNavigate();
 
+  // useEffect: 로그인 코드 처리 및 세션 설정
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
-    const code = params.get('code');
+    const code = params.get('code'); // URL에서 "code" 파라미터 가져오기
     if (code) {
+      // Cognito 토큰 교환 요청 생성
       const data = new URLSearchParams();
       data.append('grant_type', 'authorization_code');
       data.append('client_id', 'e90hcf6rica8am3h81lcsuspe');
       data.append('code', code);
-      data.append('redirect_uri', 'https://d2c37b9hnb6ap4.cloudfront.net'); 
+      data.append('redirect_uri', 'http://testdasibar.s3-website.ap-northeast-2.amazonaws.com/bongjini.html/'); 
 
       fetch('https://ap-northeast-2jczobrwlq.auth.ap-northeast-2.amazoncognito.com/oauth2/token', {
         method: 'POST',
@@ -112,6 +125,7 @@ const Navbar = () => {
       })
       .then(res => res.json())
       .then(tokens => {
+        // 토큰을 기반으로 세션 설정
         if (tokens.id_token && tokens.access_token && tokens.refresh_token) {
           const idToken = new CognitoIdToken({ IdToken: tokens.id_token });
           const accessToken = new CognitoAccessToken({ AccessToken: tokens.access_token });
@@ -122,15 +136,17 @@ const Navbar = () => {
             RefreshToken: refreshToken
           });
 
+          // Cognito 사용자 정보 설정
           const username = idToken.payload['cognito:username'];
           const user = new CognitoUser({
             Username: username,
             Pool: userPool
           });
 
+          // 사용자 세션 저장
           user.setSignInUserSession(session);
 
-          // URL에서 code 파라미터 제거
+          // URL에서 "code" 파라미터 제거
           const newUrl = window.location.origin + window.location.pathname;
           window.history.replaceState({}, document.title, newUrl);
         }
@@ -141,6 +157,7 @@ const Navbar = () => {
     }
   }, []);
 
+  // 특정 ID로 스크롤 이동
   const handleScroll = (id) => {
     const elem = document.querySelector(id);
     setClick(!click);
@@ -151,21 +168,27 @@ const Navbar = () => {
     });
   };
 
+  // BongGenie 버튼 클릭 처리
   const handleBonggenieClick = (event) => {
     event.preventDefault();
-    const user = userPool.getCurrentUser();
-    if (user) {
+    const user = userPool.getCurrentUser(); // 현재 사용자 가져오기
+    if (!user) {
+      // 사용자 세션 확인
       user.getSession((err, session) => {
         if (err || !session.isValid()) {
           alert("잘못된 접근입니다. 로그인해주세요.");
-          window.location.href = 'http://testdasibar.s3-website.ap-northeast-2.amazonaws.com/';
+          // 로그인 페이지로 리디렉션
+          window.location.href = 'https://ap-northeast-2jczobrwlq.auth.ap-northeast-2.amazoncognito.com/login?client_id=e90hcf6rica8am3h81lcsuspe&redirect_uri=https://d209iioos46fo1.cloudfront.net&response_type=code&scope=email+openid';
         } else {
-          window.location.href = '/bongjini.html';
+          // BongGenie 페이지로 이동
+          window.location.href = 'http://testdasibar.s3-website.ap-northeast-2.amazonaws.com/bongjini.html';
         }
       });
+      return;
     } else {
+      // 로그인하지 않은 경우 경고 메시지
       alert("봉지니는 로그인 후 사용하실 수 있습니다.");
-      window.location.href = 'http://testdasibar.s3-website.ap-northeast-2.amazonaws.com/';
+      window.location.href = 'https://ap-northeast-2jczobrwlq.auth.ap-northeast-2.amazoncognito.com/login?client_id=e90hcf6rica8am3h81lcsuspe&redirect_uri=https://d209iioos46fo1.cloudfront.net&response_type=code&scope=email+openid';
     }
   };
 
@@ -209,7 +232,7 @@ const Navbar = () => {
         <Item
           whileHover={{ scale: 1.1, y: -5 }}
           whileTap={{ scale: 0.9, y: 0 }}
-          onClick={() => window.location.href = 'http://testdasibar.s3-website.ap-northeast-2.amazonaws.com/'}
+          onClick={() => window.location.href = 'https://ap-northeast-2jczobrwlq.auth.ap-northeast-2.amazoncognito.com/login?client_id=e90hcf6rica8am3h81lcsuspe&redirect_uri=https://d209iioos46fo1.cloudfront.net&response_type=code&scope=email+openid'}
         >
           <Link to="#">Login / Register</Link>
         </Item>
