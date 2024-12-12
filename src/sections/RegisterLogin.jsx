61691606a550d509bb00a0c4899d1396b2c1208f
ddoljugy <ddoljugy@gmail.com>
2024-12-12 16:00:07 +0900

    v5.0.0

---
+++ src/sections/RegisterLogin.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RegisterLogin = () => {
+  return (
+    <div className="container">
+      <h1>Register & Login</h1>
+      <div className="buttons">
+        <button className="btn">Register</button>
+        <button className="btn">Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterLogin;
